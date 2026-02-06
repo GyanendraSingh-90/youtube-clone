3f7dcfe0aebc88002cbb2d45d2e655c81048cb1f
GyanendraSingh-90 <patel90shiva90@gmail.com>
2026-02-06 14:03:08 +0530

    Added Register pages with JWT handling

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import api from "../services/api";
+import { useNavigate } from "react-router-dom";
+
+export default function Register() {
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: ""
+  });
+  const navigate = useNavigate();
+
+  const handleChange = e => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const submit = async e => {
+    e.preventDefault();
+    await api.post("/auth/register", form);
+    alert("Registration successful");
+    navigate("/login");
+  };
+
+  return (
+    <form onSubmit={submit}>
+      <h2>Register</h2>
+
+      <input name="username" placeholder="Username" onChange={handleChange} />
+      <input name="email" placeholder="Email" onChange={handleChange} />
+      <input
+        name="password"
+        type="password"
+        placeholder="Password"
+        onChange={handleChange}
+      />
+
+      <button>Register</button>
+    </form>
+  );
+}
